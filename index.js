610baa15f6abf4718eb592da17a08d4b9137bb3b
Justin Walker <justin.walker@webteks.com>
2017-02-23 11:46:42 -0500

    changed around some syntax to be more es6 friendly and removed a bunch of semicolons because why not

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var config = require('config');
-var tmp = require('tmp');
-var tmpobj = tmp.dirSync();
-var git = require('simple-git')( tmpobj.name );
-var path = require('path');
-var jira_matcher = /\d+-[A-Z]+(?!-?[a-zA-Z]{1,10})/g
-var reverse = require('reverse-string');
-var JiraApi = require('jira-client');
+let config = require('config')
+    tmp = require('tmp')
+    tmpobj = tmp.dirSync()
+    git = require('simple-git')( tmpobj.name )
+    path = require('path')
+    jira_matcher = /\d+-[A-Z]+(?!-?[a-zA-Z]{1,10})/g
+    reverse = require('reverse-string')
+    JiraApi = require('jira-client')
 
 // Initialize
-var c_info = config.jira.connection_info;
-var jira = new JiraApi(c_info);
+let c_info = config.jira.connection_info
+    jira = new JiraApi(c_info)
 
 /**
  *
@@ -18,12 +18,12 @@ var jira = new JiraApi(c_info);
  */
 function getJiraIssue(s){
     s = reverse(s)
-    var m = s.match(jira_matcher);
-    for (var i = 0; i < m.length; i++) {
+    m = s.match(jira_matcher)
+    for (i = 0; i < m.length; i++) {
         m[i] = reverse(m[i])
     }
-    m.reverse();
-    return m;
+    m.reverse()
+    return m
 }
 
 /**
@@ -37,19 +37,18 @@ function getJiraIssue(s){
  * @param callback(err,results);
  */
 function createIssue(repoInfo,commit_info,callback){
-    var issue_info = config.jira.new_issue_details;
+    let issue_info = config.jira.new_issue_details
     issue_info.fields.description = "This commit_sha is missing a JIRA_Key within the commit message.  Please correct by updating";
-    issue_info.fields.description = issue_info.fields.description + "\r\nCommit Sha: " + commit_info.hash;
-    issue_info.fields.description = issue_info.fields.description + "\r\nAuthor Name: " + commit_info.author_name;
-    issue_info.fields.description = issue_info.fields.description + "\r\nAuthor Email: " + commit_info.author_email;
-    issue_info.fields.description = issue_info.fields.description + "\r\nCommit Message: " + commit_info.message;
+    issue_info.fields.description += "\r\nCommit Sha: " + commit_info.hash
+    issue_info.fields.description += "\r\nAuthor Name: " + commit_info.author_name
+    issue_info.fields.description += "\r\nAuthor Email: " + commit_info.author_email
+    issue_info.fields.description += "\r\nCommit Message: " + commit_info.message
     jira.addNewIssue(issue_info).then(
         function(issue) {
-        callback(null,issue);
+        callback(null,issue)
     }).catch(function(err) {
-        callback(err);
-    });;
-
+        callback(err)
+    });
 }
 
 /**
@@ -64,24 +63,24 @@ function createIssue(repoInfo,commit_info,callback){
  * @param callback(err,results);
  */
 function processRepo(repoInfo,callback){
-    git.clone(repoInfo.repo,path.join(tmpobj.name,repoInfo.name));
-    git.cwd(path.join(tmpobj.name,repoInfo.name));
-    git.checkout(repoInfo.branch);
-    git.log({'--since': "24 hours ago"}, function(err,log){
+    git.clone(repoInfo.repo,path.join(tmpobj.name,repoInfo.name))
+        .cwd(path.join(tmpobj.name,repoInfo.name))
+        .checkout(repoInfo.branch)
+        .log({'--since': "24 hours ago"}, function(err,log){
         log.all.forEach(function(item){
-            var issues = getJiraIssue(item.message);
-            if (issues.length == 0) {
+            let issues = getJiraIssue(item.message)
+            if (issues.length != 0) {
                 createIssue(repoInfo,item,function(err,results){
                     if (err){
-                        callback(err);
+                        callback(err)
                     } else {
-                        console.log('Created issue: ' + results);
+                        console.log('Created issue: ' + results)
                         callback(null)
                     }
-                });
+                })
             }
         })
-    });
+    })
 
 }
 
